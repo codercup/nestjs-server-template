cf615cbdc0e3f55a4be4a7f3c9aa8e5c79bcc8dc
Burt <1020103647@qq.com>
2023-06-22 13:54:19 +0800

    feat: 静态服务

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { join } from 'path';
 
 @Module({
-  imports: [SharedModule, AuthModule, UserModule],
+  imports: [
+    SharedModule,
+    AuthModule,
+    UserModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
